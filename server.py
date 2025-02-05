384212efe1d860db1a18f3e0c5339156ab094d51
Eduardo Aguilar <es123y123@gmail.com>
2025-02-05 11:53:38 -0500

    c

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def start_server(host="0.0.0.0", port=9527):
     # Evitar el error "Address already in use"
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
-    # Configuración del SO_LINGER para liberar el puerto inmediatamente después de cerrar el socket
+    # Configuración del SO_LINGER para liberar el puerto inmediatamente después de cerrar el socket.
     linger_struct = struct.pack('ii', 1, 0)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, linger_struct)
     
