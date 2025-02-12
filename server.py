9c2a4981d0f88b22383a78171d680d5016b0bf62
Eduardo Aguilar <es123y123@gmail.com>
2025-02-12 11:15:00 -0500

    c

--- server.py
+++ server.py
@@ -302,7 +302,7 @@ def start_server(host="0.0.0.0", port=9527):
                     data = client_socket.recv(1024)
                     if data:
                         print(f"Received message: {data.hex()}")
-                        process3(data.hex())
+                        # process3(data.hex())
                         # Enviar mensaje de confirmación
                         confirmation_message = response_server2(data)
                         client_socket.sendall(confirmation_message)
